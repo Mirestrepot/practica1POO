c9d92d7e5bd7862bc82a28914e2996d3402b937c
Mirestrepot <92109447+Mirestrepot@users.noreply.github.com>
2021-12-15 18:54:54 -0500

    asdf

--- practica1POO/src/uiMain/main.java
+++ practica1POO/src/uiMain/main.java
@@ -9,19 +9,11 @@ public class main {
         int opcion;
 
         do {
-            System.out.println("\t MENU PRINCIPAL"+"\n1. Realizar un pedido"+"\n2. Opcion 2"+"\n3. Opcion 3"+"\n4. Salir");
+            System.out.println("\t MENU PRINCIPAL TIENDA(X)"+"\n4. Salir");
             opcion = input.nextInt();
-            switch (opcion) {
-                case 1: GestionCliente.GestionarCliente();
-                case 2: break;
-                case 3: break;
-                case 4: break;
-                case 5: break;
-                case 6: break;
-                case 7: salirDelSistema(); break;
-            }
+            
 
-        } while (opcion != 7);
+        } while (opcion != 4);
     }
     public static void salirDelSistema() {
 
