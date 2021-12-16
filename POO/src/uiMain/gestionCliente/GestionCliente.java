796b9cda7ea04760160caa66453c0213e25bd6c1
Mirestrepot <92109447+Mirestrepot@users.noreply.github.com>
2021-12-15 19:34:52 -0500

    asdf

--- POO/src/uiMain/gestionCliente/GestionCliente.java
+++ POO/src/uiMain/gestionCliente/GestionCliente.java
@@ -10,20 +10,6 @@ public class GestionCliente {
         int opcion;
 
         do {
-            try {
-                System.out.print("\t MENU CLIENTE");
-                System.out.print("\t Para realizar un pedido... 1. Para continuar o 2. Para salir ");
-                
-                try {
-                    opcion = scan.nextInt();
-                    if (opcion == 1);
-                        RegistroCliente.registrarCliente();
-                    
-                } catch (Exception e) {
-                    System.out.print("Ingrese una opcion nuevamente: ");
-                    opcion = scan.nextInt();
-                }
-
             System.out.println("\tMENU PARA CLIENTE"+"\n1. Ingrese sus datos para hacer su pedido"+"\n2. Para realizar pedido"+"\n3. Mostrar info del Cliente");
             opcion = scan.nextInt();
             switch (opcion) {
@@ -43,5 +29,4 @@ public class GestionCliente {
 
         }while (opcion != 6);
     }
-    
-}
+}    
